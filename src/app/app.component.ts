21ca28d1c7de5205e1e0d19d8e2668fe20fe1db2
Bhavik <bhavikcpatel@hotmail.com>
2020-04-18 14:59:35 -0400

    feat: auto reload news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { timer, Observable } from 'rxjs';
+import { NewsService } from './services/news.service';
+import { News } from './models/news';
+import { tap, takeUntil, takeWhile } from 'rxjs/operators';
 @Component({
   selector: 'bp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  // 10 seconds refresh time
+  allowLoad = true;
+  refreshTimer$ = timer(0, 20000);
   title = 'rxjs-slider';
+  news$: Observable<News[]> = this.newsService.news$;
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit() {
+    this.refreshTimer$
+      .pipe(
+        takeWhile((x) => x < 3),
+        tap((_) => console.log(new Date()))
+      )
+      .subscribe(this.newsService.refresh$);
+  }
+
+  ngOnDestroy() {
+    this.allowLoad = false;
+  }
 }
---
+++ src/app/services/news.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { News } from '../models/news';
+import { tap, catchError, exhaustMap, share } from 'rxjs/operators';
+import { EMPTY, BehaviorSubject, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  constructor(private http: HttpClient) {}
+  refresh$ = new BehaviorSubject(null);
+  loadnews$ = this.http.get<News[]>('http://localhost:5201/news').pipe(
+    tap((data) => console.log('Getting Data', data)),
+    catchError((err) => {
+      console.error('Loading News', err);
+      return EMPTY;
+    }),
+    share()
+  );
+
+  news$ = this.refresh$.pipe(
+    tap((_) => console.log('Got Request')),
+    exhaustMap((_) => this.loadnews$)
+  );
+}
